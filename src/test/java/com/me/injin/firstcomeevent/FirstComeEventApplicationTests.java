0a7759370d3fbb9b948fb031c056c0a5b984d914
injinjeong <snowwwwww@naver.com>
2022-12-10 15:12:19 +0900

    first commit

---
+++ src/test/java/com/me/injin/firstcomeevent/FirstComeEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.injin.firstcomeevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstComeEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
